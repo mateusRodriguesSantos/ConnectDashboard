29c5401a25eae40baac2fad63796c1893bf2f656
Mateus Rodrigues <mateus.rteu@gmail.com>
2022-09-04 19:42:24 -0300

    Add html structure

--- App/views/partials/body.twig.php
+++ App/views/partials/body.twig.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="{{BASE}}vendor/bootstrap/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{BASE}}assets/css/bootstrap.min.css" />
     <!-- <link rel="stylesheet" href="{{BASE}}css/style.css" /> -->
     <title>
         {% block title %}{% endblock %}
