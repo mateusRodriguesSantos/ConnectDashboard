72b0043c3466f72f46fa1558bc3fb18af1c028ab
Mateus Rodrigues <mateus.rteu@gmail.com>
2022-11-15 14:37:38 -0300

    Fix menu

--- App/views/partials/header.twig.php
+++ App/views/partials/header.twig.php
@@ -1,40 +1,24 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
-          <div class="dropdown-menu">
-            <a class="dropdown-item" href="#">Action</a>
-            <a class="dropdown-item" href="#">Another action</a>
-            <a class="dropdown-item" href="#">Something else here</a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">Separated link</a>
-          </div>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
+<ul class="nav flex-column">
+  <li class="nav-item">
+    <div class="nav-user">
+      <div class="meu-perfil">
+        <h4>João</h4>
+      </div>
+      <div class="image">
+      <img class="image" src="{{BASE}}assets/img/joao.jpeg" alt="perfil" width="64" height="64">
+      </div>
     </div>
-  </div>
-</nav>
+  </li>
+  <li class="nav-item btn btn-primary" id="navButton-first">
+    <a class="nav-link active" href="#">Discipulador</a>
+  </li>
+  <li class="nav-item btn btn-primary" id="navButton">
+    <a class="nav-link" href="#">Ovelha</a>
+  </li>
+  <li class="nav-item btn btn-primary" id="navButton">
+    <a class="nav-link" href="#">Reconnect</a>
+  </li>
+  <li class="nav-item btn btn-primary" id="navButton-last">
+    <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">Sair</a>
+  </li>
+</ul>
